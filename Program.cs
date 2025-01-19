012f9d250c0707728a006c0debdecb973b4f830d
Talha ÖNDEŞ <talha_ondes@hotmail.com>
2025-01-19 16:58:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using KutuphaneUye.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<KutuphaneContext>(options =>{
+    var dosya=builder.Configuration;
+    var connectionString=dosya.GetConnectionString("database");
+    
+    options.UseSqlite(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
